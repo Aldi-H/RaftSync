fe12d6b18a6008de2b2f35fe0df7c716ed8cdadb
Aldi-H <aldihimawan88@gmail.com>
2023-07-16 12:41:10 +0700

    chore: add gestureHandler wraped in App.js

--- App.js
+++ App.js
@@ -9,13 +9,23 @@ import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import AppNavigator from './routes/HomeNavigator';
 import theme from './style/theme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from 'react-native';
 
 function App() {
   return (
-    <NativeBaseProvider theme={theme}>
-      <AppNavigator />
-    </NativeBaseProvider>
+    <GestureHandlerRootView style={styles.gestureHandlerContainer}>
+      <NativeBaseProvider theme={theme}>
+        <AppNavigator />
+      </NativeBaseProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  gestureHandlerContainer: {
+    flex: 1,
+  },
+});
 
 export default App;
