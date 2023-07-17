9ee5e2761f5123fe9bde7516d6351faad665b88e
Aldi-H <aldihimawan88@gmail.com>
2023-07-17 20:02:29 +0700

    feat: show notFound card if chart data not available

---
+++ components/charts/LineChartComponent.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Center, Flex } from 'native-base';
+import { LineChart } from 'react-native-chart-kit';
+import { Dimensions, StyleSheet } from 'react-native';
+import ChartNotFound from '../card/ChartNotFound';
+
+const LineChartComponent = ({ lineData, hidePointsAtIndex, Mode }) => {
+  if (
+    !lineData ||
+    !lineData.labels ||
+    !lineData.datasets ||
+    lineData.datasets[0].data.length === 0
+  ) {
+    return <ChartNotFound Mode={Mode} />;
+  }
+
+  return (
+    <Flex>
+      <Center>
+        {lineData.datasets[0].length > 0 ? (
+          <ChartNotFound Mode={Mode} />
+        ) : (
+          <LineChart
+            data={lineData}
+            width={Dimensions.get('window').width}
+            height={250}
+            yAxisInterval={10}
+            withDots={false}
+            bezier
+            chartConfig={{
+              backgroundColor: 'white',
+              backgroundGradientFrom: '#fbfbfb',
+              backgroundGradientTo: '#fbfbfb',
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            }}
+            hidePointsAtIndex={hidePointsAtIndex}
+            style={styles.chartContainer}
+            verticalLabelRotation={30}
+            withHorizontalLabels={false}
+            withVerticalLabels={false}
+          />
+        )}
+      </Center>
+    </Flex>
+  );
+};
+
+const styles = StyleSheet.create({
+  chartContainer: {
+    marginVertical: 8,
+    paddingLeft: 10,
+    paddingRight: 10,
+    borderRadius: 6,
+  },
+});
+
+export default LineChartComponent;
