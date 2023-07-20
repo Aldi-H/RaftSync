aa6835b0e1ee9b1d67e45715ee68287c24ad3cc7
Aldi-H <aldihimawan88@gmail.com>
2023-07-20 20:11:03 +0700

    style: add delete icon

--- components/Lists/ListComponent.js
+++ components/Lists/ListComponent.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import { VStack, Text, Box } from 'native-base';
+import { VStack, HStack, Text, Box, Pressable, DeleteIcon } from 'native-base';
 
-const ListComponent = ({ deviceName, deviceId }) => {
+const ListComponent = ({ deviceName, deviceId, onPress }) => {
   return (
     <Box bg="white" p="6" m="5" my="2" rounded="lg" borderColor="muted.50">
-      <VStack>
-        <Text
-          isTruncated
-          maxW="100%"
-          w="80%"
-          fontFamily="heading"
-          fontWeight="900"
-          fontSize="md"
-          fontStyle="normal"
-          color="coolGray.700">
-          {deviceName}
-        </Text>
-        <Text
-          fontFamily="body"
-          fontWeight="500"
-          fontSize="sm"
-          color="coolGray.600">
-          {deviceId}
-        </Text>
-      </VStack>
+      <HStack justifyContent="space-between" alignItems="center">
+        <VStack>
+          <Text
+            isTruncated
+            maxW="100%"
+            fontFamily="heading"
+            fontWeight="900"
+            fontSize="md"
+            fontStyle="normal"
+            color="coolGray.700">
+            {deviceName}
+          </Text>
+          <Text
+            fontFamily="body"
+            fontWeight="500"
+            fontSize="sm"
+            color="coolGray.600">
+            {deviceId}
+          </Text>
+        </VStack>
+        <Pressable onPress={onPress}>
+          <DeleteIcon size="md" color="red.700" />
+        </Pressable>
+      </HStack>
     </Box>
   );
 };
 
 export default ListComponent;
-
-/* <Text fontSize="md" bold color="coolGray.700">
-      evice Name
-  </Text> */
