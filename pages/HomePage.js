2ed261cf168b72e6eca39fa2cfa52baa741420cd
Aldi-H <aldihimawan88@gmail.com>
2023-07-20 20:12:51 +0700

    refactor: move all homePage state hook under state management in deviceDataStore

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,88 +1,84 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import {
-  View,
-  FlatList,
-  Pressable,
-  Fab,
-  AddIcon,
-  useDisclose,
-} from 'native-base';
+import { Pressable, Fab, AddIcon, useDisclose } from 'native-base';
+import { ScrollView, RefreshControl } from 'react-native-gesture-handler';
+import { useIsFocused } from '@react-navigation/native';
 
+import { useDeviceStore } from '../utils/deviceDataStore';
 import ListComponent from '../components/Lists/ListComponent';
-import axios from 'axios';
 import ModalComponent from '../components/Modals/ModalComponent';
 
 const HomePage = ({ navigation }) => {
-  const [devices, setDevices] = useState([]);
+  const { devices, getAllDevices, addDevice, deleteDevice } = useDeviceStore(
+    state => {
+      return {
+        devices: state.devices,
+        getAllDevices: state.getAllDevices,
+        addDevice: state.addDevice,
+        deleteDevice: state.deleteDevice,
+      };
+    },
+  );
+  const isFocussed = useIsFocused();
   const { isOpen, onOpen, onClose } = useDisclose();
+  const [refreshing, setRefreshing] = useState(false);
 
   const navigationDetails = deviceId => {
     navigation.navigate('Detail Page', { deviceId });
   };
 
-  const getAllDevices = useCallback(async () => {
-    try {
-      const response = await axios.get(
-        'https://raft-backend-kgxdguejnq-et.a.run.app/devices',
-      );
+  const handleAddDevice = async newDeviceData => {
+    await addDevice(newDeviceData);
+    getAllDevices();
+    onClose();
+  };
 
-      const data = response.data.results.map(item => {
-        return {
-          name: item.name,
-          id: item.id,
-        };
-      });
+  const handleDeleteDevice = async deviceId => {
+    await deleteDevice(deviceId);
+    getAllDevices();
+  };
 
-      console.log(data);
-      setDevices(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
-  const addDevice = useCallback(
-    async values => {
-      try {
-        const response = await axios.post(
-          'https://raft-backend-kgxdguejnq-et.a.run.app/devices',
-          values,
-        );
-
-        console.log(response.data);
-        getAllDevices();
-        onClose();
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    [getAllDevices, onClose],
-  );
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    getAllDevices();
+    setRefreshing(false);
+  }, [getAllDevices]);
 
   useEffect(() => {
     getAllDevices();
   }, [getAllDevices]);
 
   return (
-    <View>
-      <ModalComponent isOpen={isOpen} onClose={onClose} onSubmit={addDevice} />
-      <FlatList
-        mt="2"
-        data={devices}
-        renderItem={({ item }) => {
-          return (
-            <Pressable onPress={() => navigationDetails(item.id)}>
-              <ListComponent deviceName={item.name} deviceId={item.id} />
-            </Pressable>
-          );
-        }}
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }>
+      <ModalComponent
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={handleAddDevice}
       />
-      <Fab
-        renderInPortal={false}
-        onPress={() => onOpen()}
-        size="lg"
-        icon={<AddIcon name="addBtn" size="md" />}
-      />
-    </View>
+      {devices.map(item => {
+        return (
+          <Pressable key={item.id} onPress={() => navigationDetails(item.id)}>
+            <ListComponent
+              deviceName={item.name}
+              deviceId={item.id}
+              onPress={() => {
+                handleDeleteDevice(item.id);
+              }}
+            />
+          </Pressable>
+        );
+      })}
+      {isFocussed ? (
+        <Fab
+          position="absolute"
+          onPress={() => onOpen()}
+          size="lg"
+          icon={<AddIcon name="addBtn" size="md" />}
+        />
+      ) : null}
+    </ScrollView>
   );
 };
 
